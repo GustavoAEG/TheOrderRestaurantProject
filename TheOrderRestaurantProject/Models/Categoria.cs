5c97714b17eb3ede489d246aa2961347202b322e
Gustavo <p.gustavo.goncalves@viveo.com.br>
2024-05-25 19:16:11 -0300

    Add project files.

---
+++ TheOrderRestaurantProject/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace TheOrderRestaurantProject.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Prato> Pratos { get; set; } // Pratos N ----- 1 Categoria
+    }
+}
---
+++ TheOrderRestaurantProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheOrderRestaurantProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
